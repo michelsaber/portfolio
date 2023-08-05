06a952070714a8c589852a9d6724fa26b0c5c08d
Michel Saber <michel-saber@hotmail.com>
2023-08-05 08:54:15 +0200

    projectsPage done - links to be added

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import sparsam from "../icons/Sparsam.PNG";
+import festiva from "../icons/Festiva.PNG";
 
 function ProjectsPage() {
-  return <div>ProjectsPage</div>;
+  return (
+    <div className="aboutPageParent">
+      <div className="projectsPageParent">
+        <div className="sparsamDiv">
+          <h2>Sparsam</h2>
+          <img src={sparsam} style={{ width: "30vw" }} alt="" />
+        </div>
+
+        <div className="festivaDiv">
+          <h2>Festiva</h2>
+          <img src={festiva} style={{ width: "30vw" }} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsPage;
