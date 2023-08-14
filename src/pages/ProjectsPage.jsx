409eb46300236ff47d75ae004e59d5622ea18fbc
michelsaber <michel-saber@hotmail.com>
2023-08-14 14:56:55 +0200

    Responsive done

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -8,14 +8,14 @@ function ProjectsPage() {
       <div className="projectsPageParent">
         <div className="sparsamDiv">
         <a href="https://sparsam.netlify.app" className="projectLink"><h2>Sparsam</h2>
-        <img src={sparsam} style={{ width: "30vw" }} alt="" /></a>
+        <img src={sparsam} style={{ width: "30vw", height: "20vw" }} alt="" /></a>
          
         </div>
 
         <div className="festivaDiv">
         <a href="https://festiva.adaptable.app" className="projectLink">
           <h2>Festiva</h2>
-          <img src={festiva} style={{ width: "30vw" }} alt="" />
+          <img src={festiva} style={{ width: "30vw" , height: "20vw" }} alt="" />
           </a>
         </div>
       </div>
