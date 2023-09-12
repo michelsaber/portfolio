6ec2131db27535223119705ebe6b001dc7ef62ab
michelsaber <michel-saber@hotmail.com>
2023-09-12 20:30:15 +0200

    Projects Page CSS

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import sparsam from "../icons/Sparsam.PNG";
 import festiva from "../icons/Festiva.PNG";
+import { hover } from "@testing-library/user-event/dist/hover";
 
 function ProjectsPage() {
   return (
@@ -8,14 +9,14 @@ function ProjectsPage() {
       <div className="projectsPageParent">
         <div className="sparsamDiv">
         <a href="https://sparsam.netlify.app" className="projectLink"><h2>Sparsam</h2>
-        <img src={sparsam} style={{ width: "30vw", height: "20vw" }} alt="" /></a>
+        <img  src={sparsam} style={{ width: "35vw", height: "18vw" }} alt="" /></a>
          
         </div>
 
         <div className="festivaDiv">
         <a href="https://festiva.adaptable.app" className="projectLink">
           <h2>Festiva</h2>
-          <img src={festiva} style={{ width: "30vw" , height: "20vw" }} alt="" />
+          <img src={festiva} style={{ width: "35vw" , height: "18vw" }} alt="" />
           </a>
         </div>
       </div>
