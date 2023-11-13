dbd975b78812995d9cf3a90b216aea4c4667131d
michelsaber <michel-saber@hotmail.com>
2023-11-13 23:05:47 +0100

    final

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -11,7 +11,9 @@ function ProjectsPage() {
         <a href="https://sparsam.netlify.app" className="projectLink"><h2>Sparsam</h2>
         <img  src={sparsam} style={{ width: "20vw", height: "18vw" }} alt="" /></a>
          <div><p style={{paddingTop:"20px", width: "20vw", fontSize:"15px"}}>Monthly and weekly budget tracking resonsive app. React, JS, CSS, Express, Nodejs, MongoDB</p>
-         <a href="https://github.com/michelsaber/Pokemon-Canvas-Game" className="projectSourceCode"><h3>Source Code</h3></a></div>
+         <div style={{display:"flex"}}><a href="https://github.com/fraulueneburg-ironhack/p3-frontend" className="projectSourceCode"><h3>Source Frontend</h3></a></div>
+         <a href="https://github.com/fraulueneburg-ironhack/p3-backend" className="projectSourceCode"><h3>Source Backend</h3></a>
+         </div>
         </div>
 
         <div className="festivaDiv">
@@ -20,8 +22,8 @@ function ProjectsPage() {
           <img src={festiva} style={{ width: "20vw" , height: "18vw" }} alt="" />
           </a>
           <div><p style={{paddingTop:"20px", width: "20vw", fontSize:"15px"}}>Festival and events website. HTML, HBS, JS, CSS, Express, Nodejs, MongoDB</p>
-          <div style={{display:"flex"}}><a href="https://github.com/fraulueneburg-ironhack/p3-frontend" className="projectSourceCode"><h3>Frontend</h3></a>
-          <a href="https://github.com/fraulueneburg-ironhack/p3-backend" className="projectSourceCode"><h3>Backend</h3></a></div></div>
+          <a href="https://github.com/michelsaber/Ironhack-M2-Project" className="projectSourceCode"><h3>Source Code</h3></a>
+          </div>
         </div>
 
         <div className="weatherAppDiv">
