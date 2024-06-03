f35ebb6f70cc913de72d43b597356379e1ecf81a
michelsaber <michel-saber@hotmail.com>
2024-06-03 20:31:53 +0200

    Navbar adjusted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import { Row } from 'react-bootstrap'
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../icons/logo.svg";
+import navIcon1 from "../icons/nav-icon1.svg";
+import navIcon2 from "../icons/nav-icon2.svg";
+import navIcon3 from "../icons/nav-icon3.svg";
 
 function Footer() {
   return (
-    <div style={{backgroundColor :"#262f38"}} className='text-white text-center mt-5'>
-          <Row><p className='text-center'>michel-saber@hotmail.com</p></Row>
-    </div>
+    <footer className="footer pt-3" style={{backgroundColor:"#262f38"}}>
+      <Container >
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="#"><img src={navIcon1} alt="Icon" /></a>
+              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            </div>
+            <p className="text-white">Copyright 2024. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,30 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import navIcon1 from "../icons/nav-icon1.svg";
+import navIcon2 from "../icons/nav-icon2.svg";
+import navIcon3 from "../icons/nav-icon3.svg";
 
 
 function Header() {
   return (
-    <div>
-      <Navbar collapseOnSelect expand="lg" className="bg-body-secondary">
-      <Container>
-        <Navbar.Brand href="/" style={{ color:"#262f38"}} className='fw-bold'>Michel Saber</Navbar.Brand>
+    <div style={{backgroundColor:"#262f38"}}>
+      <Navbar  collapseOnSelect expand="lg"  >
+      <Container >
+        <Navbar.Brand href="/" style={{ color:"white"}} className='fw-bold'>Michel Saber</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/projects" className='text-white'>Projects</Nav.Link>
+            <Nav.Link href="/about" className='text-white'>About</Nav.Link>
             
           </Nav>
           <Nav>
-            <Nav.Link href="https://github.com/michelsaber">Github</Nav.Link>
-            <Nav.Link eventKey={2} href="https://www.linkedin.com/in/michel-saber/">
-              LinkedIn
-            </Nav.Link>
+          <div className="social-icon">
+              <a href="#"><img src={navIcon1} alt="Icon" /></a>
+              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
