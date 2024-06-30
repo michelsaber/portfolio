9a797172e49a3e6909ffbc0b24eef0b65b926ae5
michelsaber <michel-saber@hotmail.com>
2024-06-30 18:24:13 +0200

    Projects Page need adjustments still.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -21,20 +21,20 @@ function Projects() {
         </Col>
         <Col sm={6} >
             <h2 className="text-center fs-1 fw-bold" style={{color:"#262f38"}}>Sparsam</h2>
-            <p >Indulge your senses in the allure of sun-kissed sands and gentle waves. Our slice of paradise invites you to escape the ordinary and embrace the extraordinary. Immerse yourself in the tranquil beauty of our beach haven, where every moment is a promise of relaxation and pure bliss. Your dream vacation begins here.</p>
+            <p >This Page is in Progress.</p>
         <Button variant='outline-primary'>Visit</Button>
         </Col>
       </Row>
       </Container>
       <img src={wavesFooter2} style={{width:"100%", backgroundColor:"#262f38"}} alt=""/>
       <Card className='border-0'>
-      <Card.Body className=' text-white text-center fst-italic ' style={{backgroundColor:"#262f38"}}>Embark on a journey where we believe every escape deserves the best, and your extraordinary vacation starts with us.</Card.Body>
+      <Card.Body className=' text-white text-center fst-italic ' style={{backgroundColor:"#262f38"}}>Javascript, React and MongoDB.</Card.Body>
     </Card>
     <Container>
     <Row className='row align-items-center py-4' id="locations">
         <Col sm={6} >
             <h2 className="text-center fs-1 fw-bold" style={{color:"#262f38"}}>Festiva</h2>
-            <p className=' py-4 text-gray  '>Meet our passionate touristic guides, the heart and soul of your immersive journey. With a wealth of knowledge and a genuine love for the destinations they unveil, our guides transform your exploration into an insightful adventure, creating unforgettable memories at every turn. We are avalaible in Germany, France and UK.</p>   
+            <p className=' py-4 text-gray  '>This Page is in Progress.</p>   
         </Col>
         <Col sm={6} >
         <img src={projectImg2}  fluid rounded alt=""/>  
@@ -43,7 +43,7 @@ function Projects() {
       </Container>
       <img src={wavesFooter2} style={{width:"100%", backgroundColor:"#262f38"}} alt=""/>
       <Card className='border-0'>
-      <Card.Body className='text-white text-center fst-italic' style={{backgroundColor:"#262f38"}} >Step into an odyssey where we stand firm in our belief that every retreat should be nothing short of extraordinary, and your exceptional journey unfolds in our hands.</Card.Body>
+      <Card.Body className='text-white text-center fst-italic' style={{backgroundColor:"#262f38"}} >Javascript, Handlebars and MongoDB.</Card.Body>
     </Card>
     <Container>
       <Row className='row align-items-center py-4'>
@@ -52,7 +52,7 @@ function Projects() {
         </Col>
         <Col sm={6} >
         <h2 className="text-center fs-1 fw-bold" style={{color:"#262f38"}}>Vacago</h2>
-        <p className='py-4 '>Our dedicated team is committed to turning your dream vacation into a seamless reality, providing unparalleled support every step of the way. From expert guidance to personalized assistance, we are here to ensure your getaway is not only memorable but effortlessly extraordinary.</p>
+        <p className='py-4 '>This Page is in Progress.</p>
             
         </Col>
       </Row>
